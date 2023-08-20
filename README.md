73794ab36e551033b7d84d6d746d46f011bd25b1
Kairos Tay <hestiatay@gmail.com>
2023-08-20 22:31:21 +0800

    Changed to make code more verbose and have CLI usage

--- README.md
+++ README.md
@@ -1,12 +1,62 @@
-# NLTK Sentiment Analysis Tool
+# Sentiment Analysis Script
 
+This is a Python script that analyses sentiment in text data using the VADER (Valence Aware Dictionary and sEntiment Reasoner) sentiment analysis tool from the NLTK library.
 
-Download NLTK using 'pip install nltk' 
+## Table of Contents
 
-NOTE: I did NOT include ACTUAL research data in the codes due to confidentiality of respondents. You may edit the code to add in your own data to analyse. *As per requests, I have input SAMPLE responses into the student_data container*
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Command Line Arguments](#command-line-arguments)
+- [Output](#output)
+- [Dependencies](#dependencies)
+- [Contributing](#contributing)
 
-Contact me through my [LinkedIn](https://www.linkedin.com/in/kairostay/) or the student researcher's [LinkedIn](https://www.linkedin.com/in/kaeden-l-173ab026b) for more info. 
+## Description
 
-SAMPLE OUTPUT
+The script reads input data from a text file, analyses the sentiment of each text using VADER sentiment analysis, and outputs the sentiment scores and labels to a CSV file.
 
-![image](https://user-images.githubusercontent.com/80029462/233644264-5a059df6-24af-4006-b18a-2244677d0fa8.png)
+## Installation
+
+1. Clone this repository:
+
+```bash
+git clone https://github.com/Kairos-T/NLTK-Sentiment-Analyser/tree/main
+cd your-repo
+```
+
+2. Install the dependencies:
+
+```bash
+pip install -r requirements.txt
+```
+
+## Usage
+
+Run the script with the following command:
+
+```bash
+python sentiment_analysis.py input_data.txt sentiment_scores.csv
+```
+Replace sentiment_analysis.py with the name of your script, input_data.txt with the path to your input data file, and sentiment_scores.csv with the desired name of the output CSV file.
+
+### Command Line Arguments
+input_file: Path to the input text file containing the data for sentiment analysis.
+output_file: Path to the output CSV file where sentiment scores and labels will be saved.
+
+## Output
+The script will generate a CSV file containing the sentiment analysis results for each input text. The CSV file will have the following columns:
+
+Text: The input text.
+Positive: The positive sentiment score.
+Neutral: The neutral sentiment score.
+Negative: The negative sentiment score.
+Compound: The compound sentiment score.
+Sentiment Label: The label indicating the overall sentiment.
+
+## Dependencies
+Python 3.x
+NLTK
+
+## Contributing
+Contributions are welcome! If you find any issues or have suggestions for improvements, feel free to open an issue or create a pull request.
--- Sentiment-Analyser.py
+++ Sentiment-Analyser.py
@@ -1,26 +1,81 @@
 import nltk
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import csv
+import argparse
 
+# Download the VADER lexicon for sentiment analysis
 nltk.download('vader_lexicon')
+
+# Create a SentimentIntensityAnalyzer instance
 sia = SentimentIntensityAnalyzer()
 
-# Collect data from students (replace this with your own data collection process)
-student_data = [
-    "I always use strong and unique passwords for my online accounts.",
-    "I avoid sharing personal information online without proper verification.",
-    "I keep my antivirus software updated on all my devices.",
-    "I am cautious while clicking on links or downloading attachments from unknown sources.",
-    "I know how to recognize and report phishing attempts.",
-    "I understand the importance of regularly backing up my data.",
-    "I use two-factor authentication for my online accounts.",
-    "I am aware of the risks of posting personal information on social media.",
-    "I am careful about the information I share in online forums and discussion boards.",
-    "I do not share my passwords with anyone, including my friends or family."
-]
+def analyze_sentiment(input_filename, output_filename):
+    try:
+        # Read data from the input file
+        with open(input_filename, 'r') as input_file:
+            student_data = input_file.readlines()
 
-for data in student_data:
-    sentiment_scores = sia.polarity_scores(data)
-    print("Text: ", data)
-    print("Sentiment Scores: ", sentiment_scores)
-    print("Sentiment Label: ", max(sentiment_scores, key=sentiment_scores.get))
-    print("-----")
+        # Analyze sentiment and write scores to a CSV file
+        with open(output_filename, 'w', newline='') as output_file:
+            csv_writer = csv.writer(output_file)
+            # Write header row to the CSV file
+            csv_writer.writerow(['Text', 'Positive', 'Neutral', 'Negative', 'Compound', 'Sentiment Label'])
+            
+            # Initialize variables for aggregating sentiment scores
+            total_pos = total_neu = total_neg = total_compound = 0
+            total_entries = 0
+
+            for data in student_data:
+                data = data.strip().strip('"').rstrip(',')  # Remove leading/trailing whitespace and quotes
+                if data:
+                    sentiment_scores = sia.polarity_scores(data)
+                    sentiment_label = max(sentiment_scores, key=sentiment_scores.get)
+                    
+                    # Write sentiment scores and label to the CSV file
+                    csv_writer.writerow([
+                        data,
+                        format(sentiment_scores['pos'], '.2f'), 
+                        format(sentiment_scores['neu'], '.2f'), 
+                        format(sentiment_scores['neg'], '.2f'), 
+                        format(sentiment_scores['compound'], '.2f'), 
+                        sentiment_label
+                    ])
+
+                    # Aggregate sentiment scores
+                    total_pos += sentiment_scores['pos']
+                    total_neu += sentiment_scores['neu']
+                    total_neg += sentiment_scores['neg']
+                    total_compound += sentiment_scores['compound']
+                    total_entries += 1
+                else:
+                    print("Skipping empty line in input data.")
+
+            # Calculate average sentiment scores
+            avg_pos = total_pos / total_entries
+            avg_neu = total_neu / total_entries
+            avg_neg = total_neg / total_entries
+            avg_compound = total_compound / total_entries
+
+            # Write aggregated sentiment scores to the CSV file
+            csv_writer.writerow(['Aggregated Scores', format(avg_pos, '.3f'), format(avg_neu, '.3f'), format(avg_neg, '.3f'), format(avg_compound, '.3f'), ''])
+            
+        print("Sentiment scores written to", output_filename)
+
+    except FileNotFoundError:
+        print("Error: Input file '{}' not found.".format(input_filename))
+    except Exception as e:
+        print("An error occurred:", e)
+
+if __name__ == "__main__":
+    # Create an argument parser with a description for the help message
+    parser = argparse.ArgumentParser(description="Analyze sentiment of input data and output scores to a CSV file.")
+    
+    # Add positional arguments for input and output files
+    parser.add_argument("input_file", help="Path to the input text file")
+    parser.add_argument("output_file", help="Path to the output CSV file")
+
+    # Parse the command-line arguments
+    args = parser.parse_args()
+    
+    # Call the sentiment analysis function with the provided arguments
+    analyze_sentiment(args.input_file, args.output_file)
